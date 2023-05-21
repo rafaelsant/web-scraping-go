fadb45f542510d5e7f69f0bdb3b211a121051533
rafael <rafael.ribeiro.santiago@gmail.com>
2023-05-20 23:59:44 -0300

    Adding text scraping to code

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"context"
+	"log"
 
+	"github.com/chromedp/chromedp"
+)
+
+func main() {
+	url := "https://www.imdb.com/list/ls569932833/?ref_=hm_edcft_ft_lst_csesmg23_1_i"
+
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	var response string
+	err := chromedp.Run(ctx,
+		chromedp.Navigate(url),
+		chromedp.Text(`.header`, &response))
+	if err != nil {
+		log.Fatalf("error while reading %v", err)
+	}
+	println(response)
 }
