bdf5fcefb8416541c8a7ed4cf784f052a0431878
rafael <rafael.ribeiro.santiago@gmail.com>
2023-05-28 23:05:17 -0300

    create movie entity and get all information

--- main.go
+++ main.go
@@ -2,31 +2,40 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"github.com/chromedp/chromedp"
 )
+
+type Movie struct {
+	Name             string
+	Image            string
+	Description      string
+	MovieInformation string
+}
 
 func main() {
 	url := "https://www.imdb.com/list/ls569932833/?ref_=hm_edcft_ft_lst_csesmg23_1_i"
 
 	ctx, cancel := chromedp.NewContext(context.Background())
 	defer cancel()
-	var response []string
-	var images []string
-	var description []string
-	var metadata []string
-	GetInformation(url, &response, &images, &description, &metadata, ctx)
-	println(metadata[0])
+	movies := GetMovies(url, ctx)
+	fmt.Println(movies[0])
 }
 
-func GetInformation(url string, response *[]string, images *[]string, description *[]string, metadata *[]string, ctx context.Context) {
+func GetMovies(url string, ctx context.Context) []Movie {
+	var titles []string
+	var images []string
+	var description []string
+	var information []string
+	movies := make([]Movie, 100)
 	err := chromedp.Run(ctx,
 		chromedp.Navigate(url),
 		chromedp.Evaluate(`
 		var a = []
 		document.querySelectorAll(".lister-item-header").forEach(i => a.push(i.innerText))
-		a`, &response), chromedp.Evaluate(`
+		a`, &titles), chromedp.Evaluate(`
 		var a = []
 		document.querySelectorAll("div > div > div > div > a > img").forEach(i => a.push(i.src))
 		a`, &images), chromedp.Evaluate(`
@@ -36,8 +45,13 @@ func GetInformation(url string, response *[]string, images *[]string, descriptio
 		chromedp.Evaluate(`
 		var a = []
 		document.querySelectorAll("div > div > div > .text-muted").forEach(y => {if(y.innerText.startsWith('Director')){a.push(y.innerText)}})
-		a`, &metadata))
+		a`, &information))
 	if err != nil {
 		log.Fatalf("error while reading %v", err)
 	}
+
+	for num, title := range titles {
+		movies[num] = Movie{Name: title, Image: images[num], Description: description[num+1], MovieInformation: information[num]}
+	}
+	return movies
 }
